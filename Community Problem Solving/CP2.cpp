7443793785bc149b1fae0a08093111f607cdb01d
pragti-124 <122356703+pragti-124@users.noreply.github.com>
2023-05-19 13:47:22 +0530

    Create CP2.cpp

---
+++ Community Problem Solving/CP2.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int t,i=1,a,b;
+	cin>>t;
+	while(i<t+1){
+	    cin>>a>>b;
+	    i=i+1;
+	
+	    if(a<b){
+	        cout<<a<<endl;
+	    }
+	    else{
+	        cout<<b<<endl;
+	    }}
+	
+	return 0;
+}

c5ec4a674f8b28759259d59b08820fadc36c4eae
pragti-124 <122356703+pragti-124@users.noreply.github.com>
2023-06-19 21:09:21 +0530

    Create CP33.cpp

---
+++ Community Problem Solving/CP33.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int t;
+	cin>>t;
+	for(int i=0;i<t;i++){
+	    int b;
+	    cin>>b;
+	    cout<<((b/2)*(b/2)-(b/2))/2<<endl;
+	}
+	return 0;
+}

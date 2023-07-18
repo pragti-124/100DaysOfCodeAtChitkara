76b35ef3f743996cbec156bc8c6ed9ade89bbf9d
pragti-124 <122356703+pragti-124@users.noreply.github.com>
2023-07-18 22:09:49 +0530

    Create  CP62.cpp

---
+++ Community Problem Solving/CP62.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int t;
+	cin>>t;
+	while(t--)
+	{
+	    int n;
+	    cin>>n;
+	    int cupCakes = (n/2)+1;
+	    cout<<cupCakes<<endl;
+	}
+	return 0;
+}
